f90ab987281a3a3f6ff577f4d033fedf39eb27d2
Rafael Matias <rafamatias10@gmail.com>
2024-04-23 20:10:47 +0000

    Added endpoits for filtered data. Added exchangeInfo model.

--- main.py
+++ main.py
@@ -1,49 +1,76 @@
+#-------------- IMPORTS -----------------------------------------------------------
+
 import requests
 import json
 from fastapi import FastAPI
 import uvicorn
 import models
+import logging
 
+#==================================================================================
 
 app = FastAPI()
 
+logger = logger = logging.getLogger(__name__)
+
 # Root of API
+
+
+#============== API ENDPOINTS =====================================================
+
+# ---------------------------------------------------------------------------------
+# Root
 
 @app.get('/')
 def root():
-    return {"||      Bitcoin => /bitcoin     ||       Top 100 cryptos => /top100      ||        Exchanges => /exchanges     ||  "}
+    return {"|| Raw Bitcoin Data => /bitcoin || Filtered Bitcoin Data => /bitcoin/filtered || Raw Top 100 crypto data => /top100 || Filtered Top 100 crypto data => /top100/filtered || Raw Exchanges Data => /exchanges || Filtered Exchanges Data => /exchanges/filtered ||  "}
 
-# Gets Bitcoin data and dumps it into json
+
+
+# ---------------------------------------------------------------------------------
+# Raw Bitcoin Data
+
 bitcoin = 'https://api.coinlore.net/api/ticker/?id=90'
 
 @app.get("/bitcoin")
-def getBitcoin(datafile='bitcoin.json'):
+def getBitcoin():
     result = requests.get(bitcoin)
 
 
     if result.status_code == 200:
         data = result.json()
-        print('success!')
+        
         print(data)
-
-        with open(datafile, 'w') as outfile:
-            json.dump(data, outfile, indent=4)
-        print("JSON DUMPED")
-        print("JSON DUMPED")
-        print("JSON DUMPED")
-        print("JSON DUMPED")
-        print("BITCOIN")
 
         return data
     
     else:
         print(f"Error status code {result.status_code}")
 
+# ---------------------------------------------------------------------------------
+# Filtered Bitcoin Data
+
+
+@app.get("/bitcoin/filtered")
+def getBitcoinFiltered():
+
+    filteredCoins = []
+
+    coins = getBitcoin()
     
+    for coin in coins:
+
+        filteredCoin = models.coinInfo(**coin)
+        print(filteredCoin)
+        filteredCoins.append(filteredCoin)
+
+    return filteredCoins
 
 
 
-# Gets and dumps data relative to all exchanges
+# ---------------------------------------------------------------------------------
+# Raw Exchange Data
+
 allexchanges = 'https://api.coinlore.net/api/exchanges/'
 
 @app.get("/exchanges")
@@ -56,23 +83,34 @@ def allExchanges(datafile='exchanges.json'):
         print('success!')
         print(data)
 
-        with open(datafile, 'w') as outfile:
-            json.dump(data, outfile, indent=4)
-        print("JSON DUMPED")
-        print("JSON DUMPED")
-        print("JSON DUMPED")
-        print("JSON DUMPED")
-        print("ALL EXCHANGES")
-
         return data
 
     else:
         print(f"Error status code {result.status_code}")
 
-    
+# ---------------------------------------------------------------------------------
+# Filtered Exchange Data
+
+@app.get("/exchanges/filtered")
+def filterExchangeData():
+
+  filtered_exchanges = []
+
+  all_exchanges = allExchanges()
 
 
-# Gets and dumps crypto data from ETH to 101's position
+  for exchange_id, exchange_details in all_exchanges.items():
+
+    filtered_exchange = models.exchangeInfo(**exchange_details)
+    filtered_exchanges.append(filtered_exchange)
+
+
+  return filtered_exchanges
+
+# ---------------------------------------------------------------------------------
+# Raw 2-101Rank Data (ETH to 101rank)
+
+
 top100 = 'https://api.coinlore.net/api/tickers/?start=1&limit=100'
 
 @app.get("/top100")
@@ -85,18 +123,14 @@ def crypto100(datafile='top100.json'):
         print('success!')
         print(data)
 
-        with open(datafile, 'w') as outfile:
-            json.dump(data, outfile, indent=4)
-        print("JSON DUMPED")
-        print("JSON DUMPED")
-        print("JSON DUMPED")
-        print("JSON DUMPED")
-        print("TOP 100 CRYPTO")
-
         return data
     
     else:
         print(f"Error status code {result.status_code}")
+
+
+# ---------------------------------------------------------------------------------
+# Filtered 2-101Rank Data (ETH to 101rank)
 
 
 @app.get("/top100/filtered")
@@ -117,3 +151,5 @@ def filterCoinData():
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
+# ---------------------------------------------------------------------------------
--- models.py
+++ models.py
@@ -1,14 +1,5 @@
 from pydantic import BaseModel
 
-class Bitcoin(BaseModel):
-    symbol : str
-    name : str
-    rank : int
-    price_usd : float
-    market_cap_usd : float
-    volume24 : float
-    csupply : float
-
 
 class coinInfo(BaseModel):
 
@@ -23,22 +14,13 @@ class coinInfo(BaseModel):
 
 
 
-    #  "id": "80",
-    #         "symbol": "ETH",
-    #         "name": "Ethereum",
-    #         "nameid": "ethereum",
-    #         "rank": 2,
-    #         "price_usd": "3235.94",
-    #         "percent_change_24h": "1.79",
-    #         "percent_change_1h": "0.15",
-    #         "percent_change_7d": "6.99",
-    #         "price_btc": "0.048484",
-    #         "market_cap_usd": "395999687569.71",
-    #         "volume24": 10029819924.236187,
-    #         "volume24a": 9870668879.17391,
-    #         "csupply": "122375302.00",
-    #         "tsupply": "122375302",
-    #         "msupply": ""
+class exchangeInfo(BaseModel):
+
+    name : str
+    volume_usd : float
+    active_pairs : int
+    url : str
+    country : str
 
 
 
