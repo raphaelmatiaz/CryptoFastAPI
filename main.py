e9487e8614fd6c13cbec11d5c45ca47a742de794
Rafael Matias <rafamatias10@gmail.com>
2024-04-24 14:16:43 +0000

    Added 'pytz' library to project dependencies. Added logging INFO messages to endpoint functions. Added date and Lisbon-Portugal timezone time to logging messages

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 #-------------- IMPORTS -----------------------------------------------------------
 
 import requests
-import json
 from fastapi import FastAPI
 import uvicorn
 import models
 import logging
+from datetime import datetime
+from pytz import timezone
 
 #==================================================================================
+# API Instance
 
 app = FastAPI()
 
+#  -  -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   
+# Logger Instance & Config
+
+logging.basicConfig(level=logging.INFO)
+
 logger = logger = logging.getLogger(__name__)
 
-# Root of API
+#  -  -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   -   
+# Date & Time
+
+lisbon_tz = timezone('Europe/Lisbon')
+
+# current date-tiime
+now = datetime.now(lisbon_tz)
+ 
+# dd/mm/YY H:M:S
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 
 
 #============== API ENDPOINTS =====================================================
@@ -35,17 +51,35 @@ bitcoin = 'https://api.coinlore.net/api/ticker/?id=90'
 @app.get("/bitcoin")
 def getBitcoin():
     result = requests.get(bitcoin)
+    logger.info("==========================================================================")
+    logger.info(f"--------------------------- {dt_string}---------------------------")
+    logger.info(f" HTTP 'GET' request sent to CoinLore endpoint: {bitcoin}")
+    logger.info("==========================================================================")
+
 
 
     if result.status_code == 200:
+        logger.info("==========================================================================")
+        logger.info(f"--------------------------- {dt_string}---------------------------")
+        logger.info(f" REQUEST SUCCESS! Result status code : {result.status_code} - OK")
+        logger.info("==========================================================================")
+
         data = result.json()
-        
-        print(data)
+
+        logger.info("==========================================================================")
+        logger.info(f"--------------------------- {dt_string}---------------------------")
+        logger.info(f" Successfully returned latest Bitcoin data in JSON format! ")
+        logger.info(f" Check 'localhost/8000/bitcoin' to view the response! ")
+        logger.info("==========================================================================")
 
         return data
     
     else:
-        print(f"Error status code {result.status_code}")
+        logger.info("==========================================================================")
+        logger.info(f"--------------------------- {dt_string}---------------------------")
+        logger.info(f" REQUEST FAILED! Result status code : {result.status_code}")
+        logger.info(f" Failed to request latest Bitcoin data")
+        logger.info("==========================================================================")
 
 # ---------------------------------------------------------------------------------
 # Filtered Bitcoin Data
@@ -58,11 +92,23 @@ def getBitcoinFiltered():
 
     coins = getBitcoin()
     
+    logger.info("==========================================================================")
+    logger.info(f"--------------------------- {dt_string}---------------------------")
+    logger.info(f" Innitializing data filter ")
+    logger.info("--------------------------------------------------------------------------")
+    logger.info("==========================================================================")
+
     for coin in coins:
 
         filteredCoin = models.coinInfo(**coin)
-        print(filteredCoin)
+        logger.info(f" {coin['name']} data filtered successfully! ")
         filteredCoins.append(filteredCoin)
+
+        logger.info("==========================================================================")
+        logger.info(f"--------------------------- {dt_string}---------------------------")
+        logger.info(f" Successfully filtered latest Bitcoin data! ")
+        logger.info(f" Check 'localhost/8000/bitcoin/filtered' to view the filtered JSON response! ")
+        logger.info("==========================================================================")
 
     return filteredCoins
 
@@ -74,19 +120,38 @@ def getBitcoinFiltered():
 allexchanges = 'https://api.coinlore.net/api/exchanges/'
 
 @app.get("/exchanges")
-def allExchanges(datafile='exchanges.json'):
+def allExchanges():
     result = requests.get(allexchanges)
+
+    logger.info("==========================================================================")
+    logger.info(f"--------------------------- {dt_string}---------------------------")
+    logger.info(f" HTTP 'GET' request sent to CoinLore endpoint: {allexchanges}")
+    logger.info("==========================================================================")
 
 
     if result.status_code == 200:
+
+        logger.info("==========================================================================")
+        logger.info(f"--------------------------- {dt_string}---------------------------")
+        logger.info(f" REQUEST SUCCESS! Result status code : {result.status_code} - OK")
+        logger.info("==========================================================================")
+
         data = result.json()
-        print('success!')
-        print(data)
+
+        logger.info("==========================================================================")
+        logger.info(f"--------------------------- {dt_string}---------------------------")
+        logger.info(f" Successfully returned latest Exchanges data in JSON format! ")
+        logger.info(f" Check 'localhost/8000/exchanges' to view the response! ")
+        logger.info("==========================================================================")
 
         return data
 
     else:
-        print(f"Error status code {result.status_code}")
+        logger.info("==========================================================================")
+        logger.info(f"--------------------------- {dt_string}---------------------------")
+        logger.info(f" REQUEST FAILED! Result status code : {result.status_code}")
+        logger.info(f" Failed to request latest Exchanges data")
+        logger.info("==========================================================================")
 
 # ---------------------------------------------------------------------------------
 # Filtered Exchange Data
@@ -94,18 +159,30 @@ def allExchanges(datafile='exchanges.json'):
 @app.get("/exchanges/filtered")
 def filterExchangeData():
 
-  filtered_exchanges = []
+    filtered_exchanges = []
 
-  all_exchanges = allExchanges()
+    all_exchanges = allExchanges()
+
+    logger.info("==========================================================================")
+    logger.info(f"--------------------------- {dt_string}---------------------------")
+    logger.info(f" Innitializing data filter ")
+    logger.info("--------------------------------------------------------------------------")
+    logger.info("==========================================================================")
 
 
-  for exchange_id, exchange_details in all_exchanges.items():
+    for exchange_id, exchange_details in all_exchanges.items():
 
-    filtered_exchange = models.exchangeInfo(**exchange_details)
-    filtered_exchanges.append(filtered_exchange)
+        filtered_exchange = models.exchangeInfo(**exchange_details)
+        logger.info(f" {exchange_details['name']} data filtered successfully! ")
+        filtered_exchanges.append(filtered_exchange)
 
+    logger.info("==========================================================================")
+    logger.info(f"--------------------------- {dt_string}---------------------------")
+    logger.info(f" Successfully filtered latest Exchanges data! ")
+    logger.info(f" Check 'localhost/8000/exchanges/filtered' to view the filtered JSON response! ")
+    logger.info("==========================================================================")
 
-  return filtered_exchanges
+    return filtered_exchanges
 
 # ---------------------------------------------------------------------------------
 # Raw 2-101Rank Data (ETH to 101rank)
@@ -114,19 +191,38 @@ def filterExchangeData():
 top100 = 'https://api.coinlore.net/api/tickers/?start=1&limit=100'
 
 @app.get("/top100")
-def crypto100(datafile='top100.json'):
+def crypto100():
     result = requests.get(top100)
+
+    logger.info("==========================================================================")
+    logger.info(f"--------------------------- {dt_string}---------------------------")
+    logger.info(f" HTTP 'GET' request sent to CoinLore endpoint: {top100}")
+    logger.info("==========================================================================")
 
 
     if result.status_code == 200:
+
+        logger.info("==========================================================================")
+        logger.info(f"--------------------------- {dt_string}---------------------------")
+        logger.info(f" REQUEST SUCCESS! Result status code : {result.status_code} - OK")
+        logger.info("==========================================================================")
+
         data = result.json()
-        print('success!')
-        print(data)
+
+        logger.info("==========================================================================")
+        logger.info(f"--------------------------- {dt_string}---------------------------")
+        logger.info(f" Successfully returned latest Top100 altoins data in JSON format! ")
+        logger.info(f" Check 'localhost/8000/top100' to view the response! ")
+        logger.info("==========================================================================")
 
         return data
     
     else:
-        print(f"Error status code {result.status_code}")
+        logger.info("==========================================================================")
+        logger.info(f"--------------------------- {dt_string}---------------------------")
+        logger.info(f" REQUEST FAILED! Result status code : {result.status_code}")
+        logger.info(f" Failed to request latest Top100 alticoins data")
+        logger.info("==========================================================================")
 
 
 # ---------------------------------------------------------------------------------
@@ -139,17 +235,31 @@ def filterCoinData():
     filteredCoins = []
 
     coins = crypto100()['data']
+
+    logger.info("==========================================================================")
+    logger.info(f"--------------------------- {dt_string}---------------------------")
+    logger.info(f" Innitializing data filter ")
+    logger.info("--------------------------------------------------------------------------")
+    logger.info("==========================================================================")
     
     for coin in coins:
 
         filteredCoin = models.coinInfo(**coin)
-        print(filteredCoin)
+        logger.info(f" {coin['name']} data filtered successfully! ")
         filteredCoins.append(filteredCoin)
+
+    logger.info("==========================================================================")
+    logger.info(f"--------------------------- {dt_string}---------------------------")
+    logger.info(f" Successfully filtered latest Top100 altcoins data! ")
+    logger.info(f" Check 'localhost/8000/top100/filtered' to view the filtered JSON response! ")
+    logger.info("==========================================================================")
 
     return filteredCoins
 
 
+# ---------------------------------------------------------------------------------
+#APP START
+
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
 
-# ---------------------------------------------------------------------------------
